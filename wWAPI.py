968c49554fafba47e86a735355e07c7f07734640
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-13 19:42:31 +0530

    head2isusedtoshowonly2rows

--- wWAPI.py
+++ wWAPI.py
@@ -3,3 +3,9 @@ import pandas as pd
 
 response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=99fb812beef447abe0a6c14a6dc48884&language=en-US&page=1")
 #api_key = 99fb812beef447abe0a6c14a6dc48884
+
+pd.DataFrame(response.json()['results'].head(2)[['id','tittle','overview','release_date','popularity','vote_average','vote_count']])
+#response.json()['results']
+
+df= pd.DataFrame()
+
