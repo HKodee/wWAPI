adec63835ca286a97590c477aef211ee1b6c9a74
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-13 19:56:54 +0530

    The error occurs because DataFrame.append() was removed in Pandas 2.0. concat is used now instead of append

--- wWAPI.py
+++ wWAPI.py
@@ -13,6 +13,6 @@ df = pd.DataFrame()
 for i in range(1,429):
     response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=99fb812beef447abe0a6c14a6dc48884&language=en-US&page=1")
     temp_df = pd.DataFrame(response.json()['results'])[['id','title','overview','release_date','popularity','vote_average','vote_count']]
-    df.append(temp_df, ignore_index=True)
+    df = pd.concat([df, temp_df], ignore_index=True)
 
 df.to_csv('movies.csv')
