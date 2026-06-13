30b900ce5976da307c89933cf40839abc6051e45
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-13 19:54:47 +0530

    The error occurs because DataFrame.append() was removed in Pandas 2.0. That's why Python says:

--- wWAPI.py
+++ wWAPI.py
@@ -12,7 +12,7 @@ df = pd.DataFrame()
 #making a loop
 for i in range(1,429):
     response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=99fb812beef447abe0a6c14a6dc48884&language=en-US&page=1")
-    temp_df = pd.DataFrame(response.json()['results'])[['id','tittle','overview','release_date','popularity','vote_average','vote_count']]
-    df = df.append(temp_df, ignore_index=True)
+    temp_df = pd.DataFrame(response.json()['results'])[['id','title','overview','release_date','popularity','vote_average','vote_count']]
+    df.append(temp_df, ignore_index=True)
 
 df.to_csv('movies.csv')
