676761ff83cc35a876325bb5ef5a4a6821b44ced
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-13 19:34:24 +0530

    one more import

--- wWAPI.py
+++ wWAPI.py
@@ -1,3 +1,5 @@
 import requests
+import pandas as pd
 
 response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=99fb812beef447abe0a6c14a6dc48884&language=en-US&page=1")
+#api_key = 99fb812beef447abe0a6c14a6dc48884
